af18454d95f72d365f8e27349c0acefc0de910db
postbox433@gmail.com <postbox433@gmail.com>
2015-03-31 16:50:53 +0500

    add registerServices method to container

--- lib/Container.php
+++ lib/Container.php
@@ -34,6 +34,40 @@ class Container
 	{
 		static::$c = $pimple;
 	}
+
+	/**
+	 * Задает сервисы для pimple через массив
+	 * @param array $services
+	 */
+	public static function registerServices(array $services)
+	{
+		foreach ($services as $name => $config) {
+
+			if (empty($config) || (is_array($config) && empty($config['class']))) continue;
+
+			static::$c[$name] = function ($c) use ($config) {
+				$item = null;
+				if (is_array($config)) {
+					$class = $config['class'];
+					$configItem = $config;
+					unset($configItem['class']);
+					$item = new $class;
+					foreach ($configItem as $key => $value) {
+						if (property_exists($item, $key)) {
+							$item->$key = $value;
+						} else if (method_exists($item, 'set' . ucfirst($key))) {
+							$method = 'set' . ucfirst($key);
+							$item->$method($value);
+						}
+					}
+				} else {
+					$item = new $config;
+				}
+				return $item;
+			};
+			
+		}
+	}
 }
 
 
